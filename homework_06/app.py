6b539b2b5fc1cba1b1b271848dfd8ccbd3808219
benderprog <bender_prog@inbox.ru>
2023-09-23 11:13:15 +0300

    add flask app

---
+++ homework_06/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.get("/", endpoint="index")
+def get_index():
+    return render_template("index.html")
+
+
+@app.get("/about/", endpoint="about")
+def get_about():
+    return render_template("/lists/about.html")
